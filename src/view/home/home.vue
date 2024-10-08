59435e4b16b11c4baf2f2053ac81f535155a4a9a
CharlesPure <chichuang134@gmail.com>
2024-10-08 17:59:13 +0800

    init

--- src/view/home/home.vue
+++ src/view/home/home.vue
@@ -5,7 +5,9 @@ const dir = inject("dir");
 </script>
 <template>
   <div class="w100% ha p-main">
+    【即将开始。。。】
     <p v-for="i in 100" :key="i">{{ i }}</p>
+    【永无止境。。。】
   </div>
 </template>
 <style lang="scss"></style>
